54da49f9ea5e9d8ae5aa6c2a420f10f6754f412e
Sally <sallywing123@gmail.com>
2022-03-06 02:38:46 -0800

    added inquirer questions

---
+++ apple.md
@@ -0,0 +1,9 @@
+# apple 
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+[MIT](https://opensource.org/licenses/MIT)
+
+# Table of content 
+[Description](#description)
+# Description 
+apple is red 
+undefined
--- index.js
+++ index.js
@@ -12,18 +12,60 @@ const questions = [
         message:'What is your project name?'
     },
     {
+        type: 'input',
+        name: 'description',
+        message:'Please briefly describe your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message:'Please provide installation instructions.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message:'How can users utilize this project?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message:'How can users contribute to this project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message:'How can users perform testing?'
+    },
+    {
         type: 'list',
         choices:['MIT','ISC','None'],
         name: 'license',
         message:'What license is your project under?' 
-    }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address',
+    },
+    {
+        type: 'input',
+        name: 'url',
+        message: 'Please enter your deployed application URL',
+      },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, generateMarkdown(data), (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');})
+    fs.writeFile(fileName, generateMarkdown(data), (err) => 
+    {if (err) throw err;
+        console.log('The file has been saved!');
+        }
+    )
 }
 
 // TODO: Create a function to initialize app
@@ -33,7 +75,7 @@ function init() {
         .then(data =>{
             console.log(data)
             // generate README 
-            writeToFile('answers.md',data)
+            writeToFile(`${data.title}.md`,data)
         })
 }
 // Function call to initialize app
